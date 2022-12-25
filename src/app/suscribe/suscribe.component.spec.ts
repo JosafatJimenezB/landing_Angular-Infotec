cd1505313c903fa29541e6b2683a749373734fde
josafat <josa221001@gmail.com>
2022-12-25 15:22:26 -0600

    add: componentes iniciales landing

---
+++ src/app/suscribe/suscribe.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SuscribeComponent } from './suscribe.component';
+
+describe('SuscribeComponent', () => {
+  let component: SuscribeComponent;
+  let fixture: ComponentFixture<SuscribeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SuscribeComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SuscribeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/suscribe/suscribe.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-suscribe',
+  templateUrl: './suscribe.component.html',
+  styleUrls: ['./suscribe.component.css']
+})
+export class SuscribeComponent {
+
+}

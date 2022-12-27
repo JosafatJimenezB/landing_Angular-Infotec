69f7e465128383956a85d6f30a76a63466413ec0
josafat <josa221001@gmail.com>
2022-12-26 19:14:23 -0600

    add: modal del registro

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,19 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { NgbCollapseModule, NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'ngbd-collapse-navbar',
+  selector: 'ngbd-collapse-navbar, ngbd-modal-options',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: ['./navbar.component.css'],
+  styles: [
+		`
+			.dark-modal .modal-content {
+				background-color: #292b2c;
+				color: white;
+			}
+			.dark-modal .close {
+				color: white;
+			}
+			.light-blue-backdrop {
+				background-color: #5cb3fd;
+			}
+		`,
+	],
 })
 export class NavbarComponent implements OnInit{
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
   ngOnInit(): void {
 
   }
 
   isMenuCollapsed = true;
+
+  openVerticallyCentered(content:any) {
+		this.modalService.open(content, { centered: true });
+	}
 
 }
